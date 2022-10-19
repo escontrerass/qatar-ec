e14852bcd48787951bd29f76edf0e900d02af200
escontrerass <edgarsamuel.contreras@unet.edu.ve>
2022-10-19 12:31:57 -0400

    Fix menu desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,7 @@ export function Home() {
     <Layout>
       <section className='flex min-h-[80vh] flex-col items-center justify-center gap-5'>
         <picture className='w-52 rounded-lg bg-white p-2 md:w-96'>
-          <img
-            src={titleFifa}
-            alt='Fifa world cup qatar 2022 logo'
-            title='Fifa world cup qatar 2022 logo'
-            className='h-full w-full'
-          />
+          <img src={titleFifa} alt='Fifa world cup qatar 2022 logo' title='Fifa world cup qatar 2022 logo' />
         </picture>
         <picture className='w-40 md:w-80'>
           <img src={petQatar} alt='Fifa world cup qatar 2022 pet' title='Fifa world cup qatar 2022 pet' />
